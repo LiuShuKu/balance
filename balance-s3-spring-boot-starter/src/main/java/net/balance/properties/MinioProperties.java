9f78d5243d41da62090d7fc5ac9658877f2efb30
古书柳 <gushuliu@cqt01.com>
2022-05-30 18:10:56 +0800

    增加云储存相关业务

--- balance-s3-spring-boot-starter/src/main/java/net/balance/properties/MinioProperties.java
+++ balance-s3-spring-boot-starter/src/main/java/net/balance/properties/MinioProperties.java
@@ -5,9 +5,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+
 /**
- * 读取minio配置
+ * 读取minio配置 <br>
  *
+ * 官方文档: <br>
+ * 		http://docs.minio.org.cn/docs/master/java-client-quickstart-guide
  * @author : liushuku
  * @date : 30 : 05 : 2022/5/30
  */
@@ -15,44 +19,43 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "balance.s3.minio")
 public class MinioProperties {
 
-	private String url ;
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
-	public int getNum() {
-		return num;
-	}
+	/**
+	 * 对象存储服务的URL
+	 */
+	private String endpoint;
 
 	/**
-	 * 配置数量
-	 * @param num
+	 * Access key就像用户ID，可以唯一标识你的账户。
 	 */
-	public void setNum(int num) {
-		this.num = num;
+	private String accessKey;
+
+	/**
+	 * Secret key是你账户的密码。
+	 */
+	private String  secretKey;
+
+
+	public String getEndpoint() {
+		return endpoint;
 	}
 
-	private int num;
-
-	@Override
-	public String toString() {
-		return "MinioProperties{" +
-				"url='" + url + '\'' +
-				", num=" + num +
-				'}';
+	public void setEndpoint(String endpoint) {
+		this.endpoint = endpoint;
 	}
 
-	public MinioProperties(String url, int num) {
-		this.url = url;
-		this.num = num;
+	public String getAccessKey() {
+		return accessKey;
 	}
 
-	public MinioProperties() {
+	public void setAccessKey(String accessKey) {
+		this.accessKey = accessKey;
 	}
 
+	public String getSecretKey() {
+		return secretKey;
+	}
+
+	public void setSecretKey(String secretKey) {
+		this.secretKey = secretKey;
+	}
 }
