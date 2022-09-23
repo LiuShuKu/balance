b4a59df06ac8b9be0e7fff0a53cf9d3c7e347ee4
LiuShuKuBook <liushuku@yeah.net>
2022-09-23 10:18:15 +0800

    增加阿里云oss自动配置

--- balance-s3-spring-boot-starter/balance-oss-spring-boot-starter/src/main/java/net/balance/oss/autoconfigure/BalanceOssAutoConfiguration.java
+++ balance-s3-spring-boot-starter/balance-oss-spring-boot-starter/src/main/java/net/balance/oss/autoconfigure/BalanceOssAutoConfiguration.java
@@ -1,10 +1,16 @@
 package net.balance.oss.autoconfigure;
 
+import com.aliyun.oss.ClientBuilderConfiguration;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
 import net.balance.oss.properties.OssProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 /**
  * 阿里云 OSS 自动配置
@@ -18,4 +24,36 @@ public class BalanceOssAutoConfiguration {
 
 	private final Logger logger = LoggerFactory.getLogger(BalanceOssAutoConfiguration.class);
 
+	/**
+	 * 初始化OSS 配置对象
+	 */
+	@Bean
+	@Primary
+	@ConditionalOnMissingBean
+	public OssProperties ossProperties(final OssProperties properties) {
+		if (properties == null || properties.isEmpty()) {
+			logger.error("【OSS自动化配置】未检测到 oss 相关配置、如需使用请前去配置。若无需使用 请排除: BalanceOssAutoConfiguration.class");
+		}
+		return properties;
+	}
+
+	/**
+	 * 初始化OSS 客户端<br/>
+	 * 用户请不要调用 ossClient 的 shutdown()方法;<br/>
+	 * ossClient本身也封装了一个PoolingHttpClientConnectionManager对象,说明它本身就是支持链接池的,也就是可以并发访问;<br/>
+	 * ossClient对象存储在SpringIOC容器里面为单列,并不会产生OOM;
+	 */
+	@Bean
+	@ConditionalOnMissingBean
+	public OSS ossClient(final OssProperties properties) {
+		if (properties == null || properties.isEmpty()) {
+			logger.error("【OSS自动化配置】OSSClient 将无法被初始化.因为没有oss所需参数,请检查！");
+		}
+		ClientBuilderConfiguration conf = new ClientBuilderConfiguration();
+		{
+			conf.setUserAgent("balance-oss-service");
+		}
+		final OSS OssClient = new OSSClientBuilder().build(properties.getEndpoint(), properties.getAccessKeyId(), properties.getAccessKeySecret(), conf);
+		return OssClient;
+	}
 }
